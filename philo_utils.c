67b00b2ab33bd4e97665b7a0bd54e36d4510a535
byemidev <byemidev@gmail.com>
2026-02-22 12:10:17 +0100

    initial commit with some code base, no tested, pseudo code

---
+++ philo_utils.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: garevalo <garevalo@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/22 11:23:33 by garevalo          #+#    #+#             */
+/*   Updated: 2026/02/22 12:04:37 by garevalo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.c"
+
+static int	args_is_valid(char **args)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (*args[i])
+	{
+		j = 0;
+		if (args[i][j] == '-' || args[i][j] == '+')
+			j++;
+		while (args[i][j])
+		{
+			if (!is_digit(args[i][j]))
+				return (0);
+			j++;
+		}
+		i++;
+	}	
+	return (1);
+}
+
+int main(int ac, char **av)
+{
+	//TODO: 
+	// [X] check and validate the params/args
+	// [] create philo instances : create(number_of_philosophers time_to_die time_to_eat time_to_sleep )
+	// [] check error on create
+	// [] show philo data
+	
+	int i;
+	unsigned int	nro_philos;
+	unsigned long int	time_to_sleep;
+	unsigned long int	time_to_eat;
+	unsigned long int	time_to_dead;
+	typedef struct t_philos philos;
+
+	i = 0;
+	if (ac == 5)
+	{
+		if (!init_philos(&philos))
+			exit_with_error_message("cannot init philos");
+		if (!av)
+			return (0);//error handlig, NO_ARGS	
+		while (*av[i])
+		{
+			if (!args_is_valid(&av))
+				return (-1);
+			nro_philos = (unsigned int) get_nbr_value(*av[1]);
+			milli_to_sleep = (unsigned long int) get_nbr_value(*av[2]);
+			milli_to_eat = (unsigned long int) get_nbr_value(*av[3]);
+			milli_to_die = (unsigned long int) get_nbr_value(*av[4]);
+			if (!add_philo(&t_philos, nro_philos, time_to_sleep, time_to_eat, time_to_die))
+				exit_with_error_message("cannot add philo on philos");	
+			i++;
+		}
+	}
+	return (0);
+}
